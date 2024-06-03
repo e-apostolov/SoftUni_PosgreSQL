3ce91f1c0eed83720602755cc700b1f56c92b220
Evgeni Apostolov <133991275+e-apostolov@users.noreply.github.com>
2024-06-03 16:57:47 +0300

    Add files via upload

---
+++ 2024-05-31_Subqueries and JOINs Ex/05.sql
@@ -0,0 +1,8 @@
+SELECT
+	b.booking_id,
+	c.first_name AS customer_name
+FROM
+	bookings AS b,
+	customers AS c
+ORDER BY
+	customer_name
---
+++ 2024-05-31_Subqueries and JOINs Ex/13.sql
@@ -0,0 +1,22 @@
+SELECT
+	c.country_name,
+	r.river_name
+FROM
+	countries AS c
+LEFT JOIN
+	countries_rivers AS cr
+USING
+	(country_code)
+LEFT JOIN 
+	rivers AS r
+ON cr.river_id = r.id
+JOIN
+	continents AS con
+USING
+	(continent_code)
+WHERE
+	con.continent_name = 'Africa'
+ORDER BY 
+	c.country_name
+LIMIT
+	5
